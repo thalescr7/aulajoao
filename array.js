405d89137f5f848ab051dd7a708a0377c28f503c
thalescr7 <128698680+thalescr7@users.noreply.github.com>
2024-02-26 11:38:48 -0300

    Add files via upload

---
+++ array.js
@@ -0,0 +1,13 @@
+//let chamada = [1,2, 3, 4, 5, 6, 7, 8, 9]
+   // console.log(chamada[0, 8])
+
+   //let nome = ["thales", "sauer", "dentinho", "paulo", "bel"]
+   //let idade = [20, 18, 19, 16, 19]
+   //console.log(`o ${nome[0]} tem ${idade[0]} anos`)
+   //console.log(`o ${nome[1]} tem ${idade[1]} anos`)
+  // console.log(`o ${nome[2]} tem ${idade[2]} anos`)
+   //console.log(`o ${nome[3]} tem ${idade[3]} anos`)
+   //console.log(`a ${nome[4]} tem ${idade[4]} anos`)
+
+
+   
---
+++ bool.js
@@ -0,0 +1,29 @@
+//let numero1 = 10
+//let numero2 = 20
+//console.log(`o seu resultado foi: ${numero1 == numero2}`)
+
+//let cachorro = {
+   // nome: `Éros`,
+   // raca: `golden`,
+   // porte: `Grande`,
+   // bonito: `Até dmsss`
+//}
+//console.log(cachorro)
+
+//console.log(cachorro.nome)
+//console.log(cachorro.raca)
+//console.log(cachorro.porte)
+//console.log(cachorro.bonito)
+
+let pessoa = {
+    nome: `Thales`, 
+    documento: `124.932.782.09`,
+    estadocivil: `namorando a bel rutke`,
+    idade: `16`,
+    endereco: `parana`
+}
+console.log("nome:" + pessoa.nome);
+console.log("Documento:" + pessoa.documento);
+console.log("Estado civil:" + pessoa.estadocivil);
+console.log("Idade:" + pessoa.idade);
+console.log("Endereço:" + pessoa.endereco);
