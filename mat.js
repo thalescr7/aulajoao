3f2d762e11aec11af013167b20db32e52e8867e0
thalescr7 <128698680+thalescr7@users.noreply.github.com>
2024-03-04 10:29:45 -0300

    Update mat.js

---
+++ mat.js
@@ -0,0 +1,17 @@
+let soma = 5000 + 5000 + 5350;
+45
+console. log(soma); // Deve imprimir 15350
+
+46
+
+47
+let subtracao = 0 - 100 - 100 - 100 - 134 - 133;
+48
+console. log(subtracao); // Deve imprimir -567
+49
+
+
+50
+let multiplicacao = 20 * 49;
+51
+console. log(multiplicacao); // Deve imprimir 980
